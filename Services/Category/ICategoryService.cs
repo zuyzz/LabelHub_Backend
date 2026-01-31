d9a4d101b42dc8074220be4b69939e692bfd993b
nguyenhpse185023-cyber <nguyenhpse185023@fpt.edu.vn>
2026-01-31 10:05:42 +0700

    Create ICategoryService.cs

---
+++ Services/Category/ICategoryService.cs
@@ -0,0 +1,13 @@
+using DataLabel_Project_BE.DTOs.Category;
+
+namespace DataLabel_Project_BE.Services.Category
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryResponse>> GetAllAsync();
+        Task<CategoryResponse?> GetByIdAsync(Guid id);
+        Task<CategoryResponse> CreateAsync(CreateCategoryRequest request, Guid? createdBy);
+        Task<bool> UpdateAsync(Guid id, UpdateCategoryRequest request);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
