253e1a92ec30c31202f20b2162a1a837d9f6da0c
zuyzz <duyldreal@gmail.com>
2026-03-17 23:21:44 +0700

    feat: err handler middleware

---
+++ Infrastructure/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using System.Text.Json;
+
+namespace DataLabelProject.Infrastructure.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = ex switch
+            {
+                InvalidOperationException => StatusCodes.Status400BadRequest,
+                UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+                KeyNotFoundException => StatusCodes.Status404NotFound,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            var response = new
+            {
+                message = ex.Message,
+                status = context.Response.StatusCode
+            };
+
+            var json = JsonSerializer.Serialize(response);
+
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
